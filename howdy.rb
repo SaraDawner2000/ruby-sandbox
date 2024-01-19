1db2e7b3c15e79005a51b70a4120ca424b27e92c
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-01-19 21:19:19 +0000

    create debugger.rb

--- howdy.rb
+++ howdy.rb
@@ -1,5 +1,5 @@
-require "active_support/all"
-my_string = "Hello, world!"
-pp my_string
-pp "people".singularize
-require "./goodbye.rb"
+pp "What's your name?"
+
+their_name = gets.chomp
+
+puts "Hello, #{their_name}!"
